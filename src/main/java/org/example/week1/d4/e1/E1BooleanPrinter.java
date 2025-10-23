80ab5221bc715267cd9cbc11a6684c192d07281d
shivaregalla <shivarrac@gmail.com>
2025-10-24 00:33:06 +0530

    java homework week1

---
+++ src/main/java/org/example/week1/d4/e1/E1BooleanPrinter.java
@@ -0,0 +1,16 @@
+package org.example.week1.d4.e1;
+
+public class
+E1BooleanPrinter {
+    public static void main(String[] args) {
+
+        // Below is a sample code
+        boolean isTrue;
+        boolean isFalse;
+        isTrue = true;
+        isFalse = false;
+        System.out.println(isTrue);
+
+
+    }
+}
---
+++ src/main/java/org/example/week1/d4/e2/E2AgePrinter.java
@@ -0,0 +1,9 @@
+package org.example.week1.d4.e2;
+
+public class E2AgePrinter {
+    public static void main(String[] args) {
+
+
+
+    }
+}
