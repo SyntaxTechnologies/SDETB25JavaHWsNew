53470b3e1d688260bbe206ab43f93c49b50528b4
shivaregalla <shivarrac@gmail.com>
2025-11-10 22:19:38 +0530

    java homework week4

---
+++ src/main/java/org/example/week4/d4/e1/E1ForLoop.java
@@ -0,0 +1,13 @@
+package org.example.week4.d4.e1;
+
+public class E1ForLoop {
+    public static void main(String[] args) {
+
+        // Create a for loop and Initialize the loop variable to start at 2(i=2).
+        // Set the condition to continue the loop while the variable is less than or equal to 14.
+        // Increment the loop variable by 1 in each iteration.
+        // Check if the current number is even using the modulus operator (%) by using if condition
+        // If the number is even, print it to the console.
+        // Make sure your loop condition includes 14 in the range.
+    }
+}
