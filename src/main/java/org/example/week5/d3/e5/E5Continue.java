8548df4e480798796501534f8f80f232253f08d0
shivaregalla <shivarrac@gmail.com>
2025-11-17 20:41:51 +0530

    java homework week5

---
+++ src/main/java/org/example/week5/d3/e5/E5Continue.java
@@ -0,0 +1,15 @@
+package org.example.week5.d3.e5;
+
+public class E5Continue {
+    public static void main(String[] args) {
+
+        // Declare and initialize an array with sensor readings
+
+        // Enhanced for loop to iterate through the sensor readings
+
+        // If the current reading is negative, skip the current iteration using continue
+
+        // If the reading is valid, print the message for the valid reading
+
+    }
+}
---
+++ src/main/java/org/example/week5/d3/e6/E6Continue.java
@@ -0,0 +1,12 @@
+package org.example.week5.d3.e6;
+
+public class E6Continue {
+    public static void main(String[] args) {
+
+        // Enhanced for loop to iterate through the items
+
+        // If the current item is "Out of Stock", skip the current iteration using continue
+
+        // If the item is in stock, print the message for processing the item
+    }
+}
