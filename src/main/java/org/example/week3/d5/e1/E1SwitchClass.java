6f632036016be1724cdd5dfacb1862acddaece00
szambrana98 <szambrana98@gmail.com>
2025-11-17 01:23:51 -0500

    Done with the third HW!

--- src/main/java/org/example/week3/d5/e1/E1SwitchClass.java
+++ src/main/java/org/example/week3/d5/e1/E1SwitchClass.java
@@ -5,7 +5,36 @@ import java.util.Scanner;
 public class E1SwitchClass {
     public static void main(String[] args) {
 
+        Scanner input=new Scanner(System.in);
 
+        System.out.println("Enter name of the instructor");
+        String instructorName = input.nextLine();
 
+        String responsibility;
+
+        switch (instructorName) {
+            case "Asghar":
+                responsibility = "Will take care of Java Assignment";
+                break;
+
+            case "Sohail":
+                responsibility = "Will take care of SDLC Assignment";
+                break;
+
+            case "Moazzam":
+                responsibility = "Will take care of Selenium Assignment";
+                break;
+
+            case "Asef":
+                responsibility = "Will take care of every Assignment";
+                break;
+
+            default:
+                responsibility = "Invalid instructor selected";
+                break;
+        }
+
+        System.out.println(responsibility);
+        input.close();
     }
 }
