30a6fd49699537fff952f28884ca370999576ead
masixole kondile <mkondile@analytikus.co.za>
2025-11-19 13:23:27 +0200

    changes

--- src/main/java/org/example/week2/d5/e1/E1ElseIf.java
+++ src/main/java/org/example/week2/d5/e1/E1ElseIf.java
@@ -4,9 +4,32 @@ public class E1ElseIf {
     public static void main(String[] args) {
 
         // Declare a variable `day` (of type `int`) and assign it a value of 3.
+        int day = 3;
         // If day equals 1, print "Monday: Attend classes."
-        // Else if day equals 2, print "Tuesday: Library visit."
-        // Else if day equals 3, print "Wednesday: Group study session."
+        if(day == 2){
+            System.out.println("Tuesday: Library visit.");
+        }
+        else if(day == 2){
+            System.out.println("Tuesday: Library visit.");
+        }
+       else if(day == 3){
+            System.out.println("Wednesday: Group study session.");
+        }
+       else if(day == 4){
+            System.out.println("Thursday: Lab work.");
+        }
+       else if(day == 5){
+            System.out.println("Friday: Sports day.");
+        }
+       else if(day == 6){
+            System.out.println("Saturday: Free day for hobbies.");
+        }
+       else if(day == 7){
+            System.out.println("Sunday: Family time.");
+        }
+
+
+
         // Else if day equals 4, print "Thursday: Lab work."
         // Else if day equals 5, print "Friday: Sports day."
         // Else if day equals 6, print "Saturday: Free day for hobbies."
