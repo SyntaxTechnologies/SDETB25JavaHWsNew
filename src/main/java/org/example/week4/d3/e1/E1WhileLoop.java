65a42a506b0e6b686294018d4c64b37edbb66cbb
szambrana98 <szambrana98@gmail.com>
2025-11-19 17:02:15 -0500

    Done with the fourth HW!

--- src/main/java/org/example/week4/d3/e1/E1WhileLoop.java
+++ src/main/java/org/example/week4/d3/e1/E1WhileLoop.java
@@ -4,5 +4,13 @@ public class E1WhileLoop {
     public static void main(String[] args) {
 
 
+        int counter=0;
+
+        while (counter<9) {
+            System.out.println(counter);
+            counter++;
+
+        }
+
     }
 }
--- src/main/java/org/example/week4/d5/e2/E2Arrays.java
+++ src/main/java/org/example/week4/d5/e2/E2Arrays.java
@@ -3,5 +3,27 @@ package org.example.week4.d5.e2;
 public class E2Arrays {
     public static void main(String[] args) {
 
+        int[]values=new int[9];
+        values[0]=45;
+        values[1]=78;
+        values[2]=12;
+        values[3]=67;
+        values[4]=55;
+        values[5]=89;
+        values[6]=23;
+        values[7]=77;
+        values[8]=88;
+
+        int size=values.length;
+
+
+        for (int i=0; i<size; i++) {
+            if (i%2==0) {       //imporant, add to notes
+                System.out.print(values[i] + " ");
+
+            }
+        }
+
+
     }
 }
