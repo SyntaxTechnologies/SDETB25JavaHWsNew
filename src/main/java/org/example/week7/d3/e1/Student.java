a7eb73e70f0390f4e3a071bf89319b6601426f7a
masixole kondile <mkondile@analytikus.co.za>
2025-12-17 17:50:18 +0200

    masixo kondile

---
+++ src/main/java/org/example/week7/d3/e1/Student.java
@@ -0,0 +1,14 @@
+package org.example.week7.d3.e1;
+
+public class Student {
+
+    public static void main(String[] args) {
+        String name;
+        int roll_no;
+        name = "John";
+        roll_no= 2;
+
+        System.out.println("name is "+name +" and roll number is "+ roll_no);
+    }
+
+}
